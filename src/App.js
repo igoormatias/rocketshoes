175d6ce12737c04945236716f2716d338c4a36d3
Igor Matias <igorhmatias@gmail.com>
2019-09-19 15:04:24 -0300

    configurando redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GlobalStyle from './styles/global';
-
 import Header from './components/Header/index';
-
 import Routes from './routes';
+
+import store from './store';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   );
 }
