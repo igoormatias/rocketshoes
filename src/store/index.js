f0b603cc821caaa601566761f0c944f70790b1d5
Igor Matias <igorhmatias@gmail.com>
2019-09-22 16:36:30 -0300

    config: Reactotron + redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
 import { createStore } from 'redux';
+
 import rootReducer from './modules/rootReducer';
 
-const store = createStore(rootReducer);
+// Config Reactototron Redux
+const enhacer =
+  process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+
+const store = createStore(rootReducer, enhacer);
 
 export default store;
