4034df8732c6f2e6c0061765df796796f3331399
Igor Matias <igorhmatias@gmail.com>
2019-10-21 18:21:45 -0300

    config: redux-saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,23 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
-// Config Reactototron Redux
+// Config Reactototron Redux/Redux-Saga
+
+const sagaMidleware = createSagaMiddleware();
+
 const enhacer =
-  process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+  process.env.NODE_ENV === 'development'
+    ? compose(
+        console.tron.createEnhancer(),
+        applyMiddleware(sagaMidleware)
+      )
+    : applyMiddleware(sagaMidleware);
 
 const store = createStore(rootReducer, enhacer);
+
+sagaMidleware.run(rootSaga);
 
 export default store;
