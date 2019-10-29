a1c93d6eb264e9d6a7200a9f7976d9077ab00ba4
Igor Matias <igorhmatias@gmail.com>
2019-10-28 21:32:30 -0300

    feature: convertendo classe para react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
       'prettier/prettier': 'error',
@@ -34,6 +35,8 @@ module.exports = {
       ],
       'import/prefer-default-export': 'off',
       'no-param-reassign': 'off',
-      'no-console': ["error", { allow: ["tron"]}]
+      'no-console': ["error", { allow: ["tron"]}],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
   },
 };
